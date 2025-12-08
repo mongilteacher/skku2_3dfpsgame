baec7eb141ee2679544a590fca744dfce82e3e39
mongilteacher <mongilteacher@gmail.com>
2025-12-08 16:51:00 +0900

    Feat: 중력과 플레이어 점프

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -7,9 +7,15 @@ public class PlayerMove : MonoBehaviour
     // 필요 속성
     // - 이동속도
     public float MoveSpeed = 7;
-
+    // - 중력
+    public float Gravity = -9.81f;
+    // - 점프력
+    public float JumpPower = 5f;
+    
+    
     private CharacterController _controller;
-
+    private float _yVelocity = 0f;   // 중력에 의해 누적될 y값 변수
+    
     private void Awake()
     {
         _controller = GetComponent<CharacterController>();
@@ -17,6 +23,9 @@ public class PlayerMove : MonoBehaviour
 
     private void Update()
     {
+        // 0. 중력을 누적한다.
+        _yVelocity += Gravity * Time.deltaTime;
+        
         // 1. 키보드 입력 받기
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
@@ -28,9 +37,19 @@ public class PlayerMove : MonoBehaviour
         // - 글로벌 좌표 방향을 구한다. 
         Vector3 direction = new Vector3(x, 0, y);
         direction.Normalize();
+
+        
+        
+        // - 점프! : 점프 키를 누르고 && 땅이라면
+        if (Input.GetButtonDown("Jump") && _controller.isGrounded)
+        {
+            _yVelocity = JumpPower;
+        }
+        
         // - 카메라가 쳐다보는 방향으로 변환한다. (월드 -> 로컬)
         direction = Camera.main.transform.TransformDirection(direction);
-        
+        direction.y = _yVelocity; // 중력 적용
+
         
         // 3. 방향으로 이동시키기  
         _controller.Move(direction * MoveSpeed * Time.deltaTime);
