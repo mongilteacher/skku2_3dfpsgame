2946030a2a1db04f06c59b2c1a7e6f84a14f248c
mongilteacher <mongilteacher@gmail.com>
2025-12-10 13:39:50 +0900

    피격이펙트 연속 표시

--- Assets/02.Scripts/Player/PlayerGunFire.cs
+++ Assets/02.Scripts/Player/PlayerGunFire.cs
@@ -5,12 +5,12 @@ public class PlayerGunFire : MonoBehaviour
 {
     // 목표: 마우스의 왼쪽 버튼을 누르면 바라보는 방향으로 총을 발사하고 싶다. (총알을 날리고 싶다.)
     [SerializeField] private Transform _fireTransform;
-    [SerializeField] private ParticleSystem _hitEffectPrefab;
+    [SerializeField] private ParticleSystem _hitEffect;
     
     private void Update()
     {
         // 1. 마우슨 왼쪽 버튼이 눌린다면..
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(1))
         {
             // 2. Ray를 생성하고 발사할 위치, 방향, 거리를 설정한다. (쏜다.)
             Ray ray = new Ray(_fireTransform.position, Camera.main.transform.forward);
@@ -24,9 +24,16 @@ public class PlayerGunFire : MonoBehaviour
             {
                 // 5.  충돌했다면... 피격 이펙트 표시
                 Debug.Log(hitInfo.transform.name);
-                ParticleSystem hitEffect = Instantiate(_hitEffectPrefab, hitInfo.point, Quaternion.identity);
-                hitEffect.transform.forward = hitInfo.normal;
-                hitEffect.Play();
+                
+                // 파티클 생성과 플레이 방식
+                // 1. Instantiate 방식 (+ 풀링) -> 한 화면에 여러가지 수정 후 여러개 그릴경우
+                // 2. 하나를 캐싱해두고 Play     -> 인스펙터 설정 그대로 그릴 경우
+                // 3. 하나를 캐싱해두고 Emit     -> 인스펙터 설정을 수정 후 그릴 경우
+                
+                _hitEffect.transform.position = hitInfo.point;
+                _hitEffect.transform.forward = hitInfo.normal;
+
+                _hitEffect.Play();
             }
         }
         
