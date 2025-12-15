48909a455df22167afdc5bdc34fbe6565ec076d2
mongilteacher <mongilteacher@gmail.com>
2025-12-15 11:07:02 +0900

    Feat: 폰트 생성 및 게임 상태 변경

---
+++ Assets/02.Scripts/Game/EGameState.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public enum EGameState
+{
+    Ready,
+    Playing,
+    GameOver,
+}
---
+++ Assets/02.Scripts/Game/GameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private EGameState _state = EGameState.Ready;
+    public EGameState State => _state;
+
+    [SerializeField] private TextMeshProUGUI _stateTextUI;
+
+    private void Start()
+    {
+        _stateTextUI.gameObject.SetActive(true);
+
+        _state = EGameState.Ready;
+        _stateTextUI.text = "준비중...";
+
+        StartCoroutine(StartToPlay_Coroutine());
+    }
+
+    private IEnumerator StartToPlay_Coroutine()
+    {
+        yield return new WaitForSeconds(2f);
+
+        _stateTextUI.text = "시작!";
+        
+        yield return new WaitForSeconds(0.5f);
+
+        _state = EGameState.Playing;
+        
+        _stateTextUI.gameObject.SetActive(false);
+    }
+}
