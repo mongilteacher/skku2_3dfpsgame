5edcfb8f27d0a43d99521133166a76ea81a763f0
mongilteacher <mongilteacher@gmail.com>
2025-12-22 16:28:37 +0900

    Update ClickManager.cs

--- Assets/02.Scripts/ClickManager.cs
+++ Assets/02.Scripts/ClickManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 // 클릭 매니저의 역할: 게임에서 마우스 왼쪽 클릭과 오른쪽 클릭을 각각 몇 번했는지 추적하는 클래스
 public class ClickManager : MonoBehaviour
 {
+    private UI_Click ui_click;
+    
     public static ClickManager Instance;
 
     private int _leftClickCount = 0;
@@ -17,6 +19,17 @@ public class ClickManager : MonoBehaviour
     // 클릭 매니저는 구독 함수 목록을 가지고 있고, 데이터가 변경될때마다 그 함수들을 모두 호출해준다.
     public event Action OnDataChanged;
     
+    // 세분화를 얼마나 할것인가? 
+    // 너무 세분화 할수록 읽어야할 코드와 상황이 많아져서 머리가 힘들다.
+    // public event Action OnLeftClickCountChanged;
+    // public event Action OnRightClickCountChanged;
+    
+    // 레이어드 아키텍처란 시스템을 (데이터, 매니저, UI) 계층으로 나누고  의존성을 한방향으로 강제함으로써
+    // 좀 더 독립적인 구조를 지키는것.. 이를 위해서 옵저버 패턴을 쓴다.
+    
+
+    public event Action<int> OnLeftClickCountChanged;
+    
     private void Awake()
     {
         Instance = this;
@@ -27,7 +40,7 @@ public class ClickManager : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             _leftClickCount += 1;
-
+            
             OnDataChanged?.Invoke();
         }
 
